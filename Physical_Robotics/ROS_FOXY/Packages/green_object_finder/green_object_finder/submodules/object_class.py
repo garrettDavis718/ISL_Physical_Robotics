f253abe6236c5094b41f81d1cb8e2dc1af2be55f
Jeremy-Graves <132487613+Jeremy-Graves@users.noreply.github.com>
2023-07-05 12:47:10 -0400

    updated object class

--- Physical_Robotics/ROS_FOXY/Packages/green_object_finder/green_object_finder/submodules/object_class.py
+++ Physical_Robotics/ROS_FOXY/Packages/green_object_finder/green_object_finder/submodules/object_class.py
@@ -1,14 +1,15 @@
 #Creating a object class for closest objects to reference
 
 class Object():
-    def __init__(self, id, location, distance, is_green):
+    def __init__(self, id, location, distance, is_green, lidar_angles):
         self.id = id
         self.location = location
         self.distance = round(distance, 5)
         self.is_green = is_green
+        self.lidar_angles = None
 
     def __str__(self):
-        return f"id - {self.id}, loc - {self.location}, dist - {self.distance}, isGreen - {self.is_green}"
+        return f"id - {self.id}, loc - {self.location}, dist - {self.distance}, isGreen - {self.is_green}, angles - {self.lidar_angles}"
     
     def object_to_tuple(self):
-        return (self.id, self.location, self.distance, self.is_green)
+        return (self.id, self.location, self.distance, self.is_green, self.lidar_angles)
